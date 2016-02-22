99a33e34932168d1fdb1bfe06f211196d707a084
Jonas Deitmerg <jonasdeitmerg@aim.com>
2016-02-22 17:49:04 +0100

    dht-lib: Fix conflictive comments

--- firmware/src/dht.c
+++ firmware/src/dht.c
@@ -59,7 +59,7 @@ int8_t dht_getdata(int8_t *temperature, int8_t *humidity) {
 		uint8_t result=0;
 		for(i=0; i<8; i++) {//read every bit
 			timeoutcounter = 0;
-			while(!(DHT_PIN & (1<<DHT_INPUTPIN))) { //wait for an high input (non blocking)
+			while(!(DHT_PIN & (1<<DHT_INPUTPIN))) { //wait for an high input (blocking)
 				timeoutcounter++;
 				if(timeoutcounter > DHT_TIMEOUT) {
 					return -1; //timeout
@@ -69,7 +69,7 @@ int8_t dht_getdata(int8_t *temperature, int8_t *humidity) {
 			if(DHT_PIN & (1<<DHT_INPUTPIN)) //if input is high after 30 us, get result
 				result |= (1<<(7-i));
 			timeoutcounter = 0;
-			while(DHT_PIN & (1<<DHT_INPUTPIN)) { //wait until input get low (non blocking)
+			while(DHT_PIN & (1<<DHT_INPUTPIN)) { //wait until input goes low (blocking)
 				timeoutcounter++;
 				if(timeoutcounter > DHT_TIMEOUT) {
 					return -1; //timeout
@@ -81,7 +81,7 @@ int8_t dht_getdata(int8_t *temperature, int8_t *humidity) {
 
 	//reset port
 	DHT_DDR |= (1<<DHT_INPUTPIN); //output
-	DHT_PORT |= (1<<DHT_INPUTPIN); //low
+	DHT_PORT |= (1<<DHT_INPUTPIN); //high
 
 	//check checksum
 	if ((uint8_t)(bits[0] + bits[1] + bits[2] + bits[3]) == bits[4]) {
